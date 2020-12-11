fd2af5c653225de29281dbfa063f077343f52580
matsarat <mat.saratowicz@gmail.com>
2020-12-11 21:29:23 +0100

    Add message-printing method

--- src/main/java/com/example/java/maven/ticketVendingMachine/MessagePrinter.java
+++ src/main/java/com/example/java/maven/ticketVendingMachine/MessagePrinter.java
@@ -1,4 +1,9 @@
 package com.example.java.maven.ticketVendingMachine;
 
 public class MessagePrinter {
+
+    public void printMessage(String messageToPrint) {
+        System.out.println(messageToPrint);
+    }
+
 }
