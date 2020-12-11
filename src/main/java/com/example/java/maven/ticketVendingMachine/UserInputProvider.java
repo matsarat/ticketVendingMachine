504ca3b59f68051d7cb22f35af19c98c4f2f8a06
matsarat <mat.saratowicz@gmail.com>
2020-12-11 21:28:21 +0100

    Add methods asking for type of ticket and coins

--- src/main/java/com/example/java/maven/ticketVendingMachine/UserInputProvider.java
+++ src/main/java/com/example/java/maven/ticketVendingMachine/UserInputProvider.java
@@ -1,7 +1,24 @@
 package com.example.java.maven.ticketVendingMachine;
 
+import java.util.Scanner;
+
 public class UserInputProvider {
 
+    MessagePrinter messagePrinter = new MessagePrinter();
+
+    public String getTypeOfTicket() {
+        messagePrinter.printMessage("" + '\n' +
+                "What kind of ticket would you like to buy? ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public Integer getCoins() {
+        messagePrinter.printMessage("" + '\n' +
+                "Insert coins:? ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
 
 
 }
