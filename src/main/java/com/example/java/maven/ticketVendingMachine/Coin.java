7de4336f0be54b4881ccdb96af8444f2066624da
matsarat <mat.saratowicz@gmail.com>
2020-12-12 22:21:36 +0100

    Change coin values to ints and edit string interpretations of coins

--- src/main/java/com/example/java/maven/ticketVendingMachine/Coin.java
+++ src/main/java/com/example/java/maven/ticketVendingMachine/Coin.java
@@ -1,17 +1,17 @@
 package com.example.java.maven.ticketVendingMachine;
 
 public enum Coin {
-    FIVEZL(5.0f, "5 zl"),
-    TWOZL(2.0f, "2 zl" ),
-    ONEZL(1.0f, "1 zl"),
-    FIFTYGR(0.50f, "50 gr"),
-    TWENTYGR(0.20f, "20 gr"),
-    TENGR(0.10f, "10 gr");
+    FIVEZL(500, "5 PLN"),
+    TWOZL(200, "2 PLN" ),
+    ONEZL(100, "1 PLN"),
+    FIFTYGR(50, "0,50 PLN"),
+    TWENTYGR(20, "0,20 PLN"),
+    TENGR(10, "0,10 PLN");
 
-    private final float value;
+    private final int value;
     private final String symbol;
 
-    Coin(float value, String symbol) {
+    Coin(int value, String symbol) {
         this.value = value;
         this.symbol = symbol;
     }
