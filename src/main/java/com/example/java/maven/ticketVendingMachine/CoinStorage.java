6a65f095a287fd0760cae1ef3658d2d7c56eb9a0
matsarat <mat.saratowicz@gmail.com>
2020-12-12 21:07:59 +0100

    Create CoinStorage class

---
+++ src/main/java/com/example/java/maven/ticketVendingMachine/CoinStorage.java
@@ -0,0 +1,12 @@
+package com.example.java.maven.ticketVendingMachine;
+
+import java.util.List;
+
+public class CoinStorage {
+    private final List<Coin> coinStorage;
+
+    public CoinStorage(List<Coin> coinStorage) {
+        this.coinStorage = coinStorage;
+    }
+    
+}
