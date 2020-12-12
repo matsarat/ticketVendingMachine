e73c092b04d3506c4d22a192162d5cea98ffcb2c
matsarat <mat.saratowicz@gmail.com>
2020-12-12 21:12:28 +0100

    Add getters for Coin class fields

--- src/main/java/com/example/java/maven/ticketVendingMachine/Coin.java
+++ src/main/java/com/example/java/maven/ticketVendingMachine/Coin.java
@@ -1,9 +1,9 @@
 package com.example.java.maven.ticketVendingMachine;
 
 public enum Coin {
-    FIVEZL(5.0f, "5 zł"),
-    TWOZL(2.0f, "2 zł" ),
-    ONEZL(1.0f, "1 zł"),
+    FIVEZL(5.0f, "5 zl"),
+    TWOZL(2.0f, "2 zl" ),
+    ONEZL(1.0f, "1 zl"),
     FIFTYGR(0.50f, "50 gr"),
     TWENTYGR(0.20f, "20 gr"),
     TENGR(0.10f, "10 gr");
@@ -14,5 +14,13 @@ public enum Coin {
     Coin(float value, String symbol) {
         this.value = value;
         this.symbol = symbol;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public String getSymbol() {
+        return symbol;
     }
 }
