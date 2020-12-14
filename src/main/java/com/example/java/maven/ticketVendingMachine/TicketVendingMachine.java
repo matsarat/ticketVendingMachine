7b0adc8197538126fc9bde2241a043c384b373b5
matsarat <mat.saratowicz@gmail.com>
2020-12-14 14:51:52 +0100

    Add getTicket method and delete redundant field

--- src/main/java/com/example/java/maven/ticketVendingMachine/TicketVendingMachine.java
+++ src/main/java/com/example/java/maven/ticketVendingMachine/TicketVendingMachine.java
@@ -2,20 +2,16 @@ package com.example.java.maven.ticketVendingMachine;
 
 
 public class TicketVendingMachine {
-    private final Ticket ticket;
     private final TicketPrinter ticketPrinter;
     private final TicketStorage ticketStorage;
     private final MessagePrinter messagePrinter;
     private final UserInputProvider userInputProvider;
 
-    public TicketVendingMachine(Coin coin,
-                                Ticket ticket,
-                                TicketPrinter ticketPrinter,
+    public TicketVendingMachine(TicketPrinter ticketPrinter,
                                 TicketStorage ticketStorage,
                                 MessagePrinter messagePrinter,
                                 UserInputProvider userInputProvider) {
 
-        this.ticket = ticket;
         this.ticketPrinter = ticketPrinter;
         this.ticketStorage = ticketStorage;
         this.messagePrinter = messagePrinter;
@@ -30,6 +26,16 @@ public class TicketVendingMachine {
         } catch (IllegalArgumentException exception) {
             messagePrinter.printError(exception.getMessage());
             return getCoin();
+        }
+    }
+
+    public Ticket getTicket() {
+        String userInput = userInputProvider.getTypeOfTicket();
+        try {
+            return Ticket.matchUserInput(userInput.toUpperCase());
+        } catch (IllegalArgumentException exception) {
+            messagePrinter.printError(exception.getMessage());
+            return getTicket();
         }
     }
 
