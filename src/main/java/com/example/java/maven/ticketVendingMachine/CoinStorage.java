be2e1aaa0ff16f408d7ce2aa33168c074ec98946
matsarat <mat.saratowicz@gmail.com>
2020-12-15 21:46:50 +0100

    Change temp and main coin storages to Map

--- src/main/java/com/example/java/maven/ticketVendingMachine/CoinStorage.java
+++ src/main/java/com/example/java/maven/ticketVendingMachine/CoinStorage.java
@@ -1,12 +1,36 @@
 package com.example.java.maven.ticketVendingMachine;
 
-import java.util.List;
+import java.util.Map;
 
 public class CoinStorage {
-    private final List<Coin> coinStorage;
+    private final Map<Coin, Integer> mainCoinStorage;
+    private final Map<Coin, Integer> tempCoinStorage;
+    private final UserInputProvider userInputProvider;
 
-    public CoinStorage(List<Coin> coinStorage) {
-        this.coinStorage = coinStorage;
+
+    public CoinStorage(Map<Coin, Integer> mainCoinStorage,
+                       Map<Coin, Integer> tempCoinStorage,
+                       UserInputProvider userInputProvider) {
+        this.mainCoinStorage = mainCoinStorage;
+        this.tempCoinStorage = tempCoinStorage;
+        this.userInputProvider = userInputProvider;
     }
-    
+
+    public Map<Coin, Integer> getMainCoinStorage() {
+        return mainCoinStorage;
+    }
+
+    public Map<Coin, Integer> getTempCoinStorage() {
+        return tempCoinStorage;
+    }
+
+
+    public static int getValueOfCoinsInStorage(Map<Coin, Integer> coinStorage) {
+        int valueOfCoinsInStorage = 0;
+        for (Map.Entry<Coin, Integer> coinEntry : coinStorage.entrySet()) {
+            valueOfCoinsInStorage += coinEntry.getValue();
+        }
+        return valueOfCoinsInStorage;
+    }
+
 }
