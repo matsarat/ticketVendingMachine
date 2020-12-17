3c7efd2e66bd34cf90b583b8d240e17b4b8f195e
matsarat <mat.saratowicz@gmail.com>
2020-12-17 12:51:42 +0100

    Add methods used by giving back odd money logic

--- src/main/java/com/example/java/maven/ticketVendingMachine/TicketVendingMachine.java
+++ src/main/java/com/example/java/maven/ticketVendingMachine/TicketVendingMachine.java
@@ -1,8 +1,8 @@
 package com.example.java.maven.ticketVendingMachine;
 
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TicketVendingMachine {
     private final TicketPrinter ticketPrinter;
@@ -54,12 +54,64 @@ public class TicketVendingMachine {
     }
 
     public void giveBackOddMoney() {
-        List<Coin> oddCoinsToGiveBack = new ArrayList<>();
+        Map<Coin, Integer> oddCoinsToGiveBackStorage = new HashMap<>();
         int ticketsPrice = ticketStorage.getValueOfTicketsInStorage();
         int valueOfCoinsInTempStorage = CoinStorage.getValueOfCoinsInStorage(coinStorage.getTempCoinStorage());
         int oddMoney = valueOfCoinsInTempStorage - ticketsPrice;
+        int valueOfCoinsToGiveBackStorage = 0;
+        while (valueOfCoinsToGiveBackStorage != oddMoney) {
+            for (int value : Coin.getAllCoinValues()) {
+                int requiredNumberOfCoinsWithGivenValue = getRequiredNumberOfCoinsWithGivenValue(oddMoney, value);
+                if (requiredNumberOfCoinsWithGivenValue > 0) {
+                    if (areRequiredCoinsAvailableInCoinStorage(requiredNumberOfCoinsWithGivenValue, value)) {
+                        getCoinFromCoinStorageToGiveBack(requiredNumberOfCoinsWithGivenValue, value, oddCoinsToGiveBackStorage);
+                        valueOfCoinsToGiveBackStorage = getValueOfCoinsToGiveBackStorage(oddCoinsToGiveBackStorage);
+                    }
+                }
+            }
+        }
+    }
 
+    public int getValueOfCoinsToGiveBackStorage(Map<Coin, Integer> oddCoinsToGiveBack) {
+        int valueOfCoinsToGiveBackStorage = 0;
+        for (Map.Entry<Coin, Integer> coinEntry : oddCoinsToGiveBack.entrySet()) {
+            valueOfCoinsToGiveBackStorage += coinEntry.getValue();
+        }
+        return valueOfCoinsToGiveBackStorage;
+    }
 
+    public void getCoinFromCoinStorageToGiveBack(int numberOfCoinsWithGivenValue, int value, Map<Coin, Integer> oddCoinsToGiveBackStorage) {
+        for (int i = 0; i < numberOfCoinsWithGivenValue; i++) {
+            for (Map.Entry<Coin, Integer> coinEntry : coinStorage.getMainCoinStorage().entrySet()) {
+                if (coinEntry.getValue() == value) {
+                    oddCoinsToGiveBackStorage.put(coinEntry.getKey(), coinEntry.getValue());
+                    coinStorage.getMainCoinStorage().entrySet().remove(coinEntry);
+                }
+            }
+        }
+    }
+
+    public int getRequiredNumberOfCoinsWithGivenValue(int oddMoney, int coinValue) {
+        return Math.floorDiv(oddMoney, coinValue);
+    }
+
+    public boolean areRequiredCoinsAvailableInCoinStorage(int requiredNumberOfCoins, int coinValue) {
+        if (getNumberOfCoinsWithGivenValueInMainCoinStorage(coinValue) >= requiredNumberOfCoins) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public int getNumberOfCoinsWithGivenValueInMainCoinStorage(int coinValue) {
+        int coinCounter = 0;
+        for (Map.Entry<Coin, Integer> coinEntry : coinStorage.getMainCoinStorage().entrySet()) {
+            if (coinEntry.getValue() == coinValue) {
+                coinCounter++;
+            }
+        }
+        return coinCounter;
     }
 
     public void getPayment() {
