4225ef051a152acabe399485e6449f5d19998e49
matsarat <mat.saratowicz@gmail.com>
2021-01-26 20:04:02 +0100

    Delete unused fields and methods, add method moving coins from temp to main storage

--- src/main/java/com/example/java/maven/ticketVendingMachine/CoinStorage.java
+++ src/main/java/com/example/java/maven/ticketVendingMachine/CoinStorage.java
@@ -1,36 +1,43 @@
 package com.example.java.maven.ticketVendingMachine;
 
 import java.util.Map;
+import java.util.Optional;
 
 public class CoinStorage {
-    private final Map<Coin, Integer> mainCoinStorage;
-    private final Map<Coin, Integer> tempCoinStorage;
-    private final UserInputProvider userInputProvider;
+    private final Map<Coin, Integer> coins;
 
-
-    public CoinStorage(Map<Coin, Integer> mainCoinStorage,
-                       Map<Coin, Integer> tempCoinStorage,
-                       UserInputProvider userInputProvider) {
-        this.mainCoinStorage = mainCoinStorage;
-        this.tempCoinStorage = tempCoinStorage;
-        this.userInputProvider = userInputProvider;
+    public CoinStorage(Map<Coin, Integer> coins) {
+        this.coins = coins;
     }
 
-    public Map<Coin, Integer> getMainCoinStorage() {
-        return mainCoinStorage;
+    public Map<Coin, Integer> getCoins() {
+        return coins;
     }
 
-    public Map<Coin, Integer> getTempCoinStorage() {
-        return tempCoinStorage;
+    public void clear() {
+        coins.clear();
     }
 
+    public void addCoin(Coin coin) {
+        int currentNumberOfCoins = Optional.ofNullable(coins.get(coin)).orElse(0);
+        coins.put(coin, currentNumberOfCoins + 1);
+    }
+
+    public static void moveCoinsFromTempToMainCoinStorage(CoinStorage mainCoinStorage, CoinStorage tempCoinStorage) {
+        for (Map.Entry<Coin, Integer> coin : tempCoinStorage.getCoins().entrySet()) {
+            int numberOfCoinsInTempStorage = coin.getValue();
+            int numberOfCoinsInMainStorage = Optional.ofNullable(mainCoinStorage.getCoins().get(coin.getValue())).orElse(0);
+            mainCoinStorage.getCoins().put(coin.getKey(), numberOfCoinsInMainStorage + numberOfCoinsInTempStorage);
+
+        }
+        tempCoinStorage.clear();
+    }
 
     public static int getValueOfCoinsInStorage(Map<Coin, Integer> coinStorage) {
         int valueOfCoinsInStorage = 0;
-        for (Map.Entry<Coin, Integer> coinEntry : coinStorage.entrySet()) {
-            valueOfCoinsInStorage += coinEntry.getValue();
+        for (Coin coin : coinStorage.keySet()) {
+            valueOfCoinsInStorage += (coin.getCoinValue() * coinStorage.get(coin));
         }
         return valueOfCoinsInStorage;
     }
-
 }
