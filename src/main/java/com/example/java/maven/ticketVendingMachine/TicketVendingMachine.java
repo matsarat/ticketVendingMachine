755aaffafef11e4c4019363e47cc7e0d0a213f19
matsarat <mat.saratowicz@gmail.com>
2021-01-29 19:10:55 +0100

    Delete methods moved to CoinStorage and update giving back odd money

--- src/main/java/com/example/java/maven/ticketVendingMachine/TicketVendingMachine.java
+++ src/main/java/com/example/java/maven/ticketVendingMachine/TicketVendingMachine.java
@@ -1,25 +1,28 @@
 package com.example.java.maven.ticketVendingMachine;
 
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class TicketVendingMachine {
     private final TicketPrinter ticketPrinter;
     private final TicketStorage ticketStorage;
-    private final CoinStorage coinStorage;
+    private final CoinStorage mainCoinStorage;
+    private final CoinStorage tempCoinStorage;
+    private final CoinStorage oddMoneyStorage;
     private final MessagePrinter messagePrinter;
     private final UserInputProvider userInputProvider;
 
     public TicketVendingMachine(TicketPrinter ticketPrinter,
                                 TicketStorage ticketStorage,
-                                CoinStorage coinStorage,
+                                CoinStorage mainCoinStorage,
+                                CoinStorage tempCoinStorage,
+                                CoinStorage oddMoneyStorage,
                                 MessagePrinter messagePrinter,
                                 UserInputProvider userInputProvider) {
 
         this.ticketPrinter = ticketPrinter;
         this.ticketStorage = ticketStorage;
-        this.coinStorage = coinStorage;
+        this.mainCoinStorage = mainCoinStorage;
+        this.tempCoinStorage = tempCoinStorage;
+        this.oddMoneyStorage = oddMoneyStorage;
         this.messagePrinter = messagePrinter;
         this.userInputProvider = userInputProvider;
 
@@ -44,79 +47,27 @@ public class TicketVendingMachine {
             return getTicket();
         }
     }
-
-    public void addCoinToTempCoinStorage() {
-        coinStorage.getTempCoinStorage().put(getCoin(), getCoin().getValue());
-    }
-
-    public void moveCoinsFromTempToMainCoinStorage() {
-        coinStorage.getMainCoinStorage().putAll(coinStorage.getTempCoinStorage());
-    }
-
     public void giveBackOddMoney() {
-        Map<Coin, Integer> oddCoinsToGiveBackStorage = new HashMap<>();
         int ticketsPrice = ticketStorage.getValueOfTicketsInStorage();
-        int valueOfCoinsInTempStorage = CoinStorage.getValueOfCoinsInStorage(coinStorage.getTempCoinStorage());
+        int valueOfCoinsInTempStorage = CoinStorage.getValueOfCoinsInStorage(tempCoinStorage);
         int oddMoney = valueOfCoinsInTempStorage - ticketsPrice;
         int valueOfCoinsToGiveBackStorage = 0;
         while (valueOfCoinsToGiveBackStorage != oddMoney) {
-            for (int value : Coin.getAllCoinValues()) {
-                int requiredNumberOfCoinsWithGivenValue = getRequiredNumberOfCoinsWithGivenValue(oddMoney, value);
-                if (requiredNumberOfCoinsWithGivenValue > 0) {
-                    if (areRequiredCoinsAvailableInCoinStorage(requiredNumberOfCoinsWithGivenValue, value)) {
-                        getCoinFromCoinStorageToGiveBack(requiredNumberOfCoinsWithGivenValue, value, oddCoinsToGiveBackStorage);
-                        valueOfCoinsToGiveBackStorage = getValueOfCoinsToGiveBackStorage(oddCoinsToGiveBackStorage);
+            for (Coin coin : Coin.values()) {
+                int requiredNumberOfGivenCoins = CoinStorage.getRequiredNumberOfCoinsWithGivenValue(oddMoney, coin.getCoinValue());
+                if (requiredNumberOfGivenCoins > 0) {
+                    if (CoinStorage.areRequiredCoinsAvailableInCoinStorage(requiredNumberOfGivenCoins, coin.getCoinValue(), mainCoinStorage)) {
+                        CoinStorage.moveRequiredNumberOfCoinsFromMainStorageToOddMoneyStorage(mainCoinStorage, oddMoneyStorage, requiredNumberOfGivenCoins, coin);
+                        valueOfCoinsToGiveBackStorage = CoinStorage.getValueOfCoinsInStorage(oddMoneyStorage);
                     }
                 }
             }
         }
     }
 
-    public int getValueOfCoinsToGiveBackStorage(Map<Coin, Integer> oddCoinsToGiveBack) {
-        int valueOfCoinsToGiveBackStorage = 0;
-        for (Map.Entry<Coin, Integer> coinEntry : oddCoinsToGiveBack.entrySet()) {
-            valueOfCoinsToGiveBackStorage += coinEntry.getValue();
-        }
-        return valueOfCoinsToGiveBackStorage;
-    }
-
-    public void getCoinFromCoinStorageToGiveBack(int numberOfCoinsWithGivenValue, int value, Map<Coin, Integer> oddCoinsToGiveBackStorage) {
-        for (int i = 0; i < numberOfCoinsWithGivenValue; i++) {
-            for (Map.Entry<Coin, Integer> coinEntry : coinStorage.getMainCoinStorage().entrySet()) {
-                if (coinEntry.getValue() == value) {
-                    oddCoinsToGiveBackStorage.put(coinEntry.getKey(), coinEntry.getValue());
-                    coinStorage.getMainCoinStorage().entrySet().remove(coinEntry);
-                }
-            }
-        }
-    }
-
-    public int getRequiredNumberOfCoinsWithGivenValue(int oddMoney, int coinValue) {
-        return Math.floorDiv(oddMoney, coinValue);
-    }
-
-    public boolean areRequiredCoinsAvailableInCoinStorage(int requiredNumberOfCoins, int coinValue) {
-        if (getNumberOfCoinsWithGivenValueInMainCoinStorage(coinValue) >= requiredNumberOfCoins) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    public int getNumberOfCoinsWithGivenValueInMainCoinStorage(int coinValue) {
-        int coinCounter = 0;
-        for (Map.Entry<Coin, Integer> coinEntry : coinStorage.getMainCoinStorage().entrySet()) {
-            if (coinEntry.getValue() == coinValue) {
-                coinCounter++;
-            }
-        }
-        return coinCounter;
-    }
-
     public void getPayment() {
-        while (ticketStorage.getValueOfTicketsInStorage() > CoinStorage.getValueOfCoinsInStorage(coinStorage.getTempCoinStorage())) {
-            addCoinToTempCoinStorage();
+        while (ticketStorage.getValueOfTicketsInStorage() > CoinStorage.getValueOfCoinsInStorage(tempCoinStorage)) {
+            tempCoinStorage.addCoin(getCoin());
         }
     }
 }
