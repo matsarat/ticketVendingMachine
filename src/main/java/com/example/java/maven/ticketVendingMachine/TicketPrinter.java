f43eefddf808730bddea776f2f52cb4e1fac2f52
matsarat <mat.saratowicz@gmail.com>
2021-02-02 21:01:13 +0100

    Apply change in ticket-printing method to avoid infinite printing

--- src/main/java/com/example/java/maven/ticketVendingMachine/TicketPrinter.java
+++ src/main/java/com/example/java/maven/ticketVendingMachine/TicketPrinter.java
@@ -3,7 +3,7 @@ package com.example.java.maven.ticketVendingMachine;
 import java.util.Map;
 
 public class TicketPrinter {
-    private int paperContainerState;
+    private static int paperContainerState;
     private final static String PRINT_TICKETS = "Here are your tickets:";
     private final MessagePrinter messagePrinter;
 
@@ -12,11 +12,11 @@ public class TicketPrinter {
         this.messagePrinter = messagePrinter;
     }
 
-    public int getPaperContainerState() {
+    public static int getPaperContainerState() {
         return paperContainerState;
     }
 
-    public boolean isPaperInPrinter() {
+    public static boolean isPaperInPrinter() {
         if (paperContainerState > 0) {
             return true;
         }
@@ -25,20 +25,22 @@ public class TicketPrinter {
         }
     }
 
-    public boolean isTicketPrintingPossible(TicketStorage ticketStorage) {
+    public static boolean isTicketPrintingPossible(TicketStorage ticketStorage) {
         if (paperContainerState >= ticketStorage.getTickets().size()) {
             return true;
         }
         else return false;
     }
 
-    public void printTicketsFromTicketStorage(TicketStorage ticketStorage) {
+    public static void printTicketsFromTicketStorage(TicketStorage ticketStorage) {
+        MessagePrinter messagePrinter = new MessagePrinter();
         messagePrinter.printMessage(PRINT_TICKETS);
         for (Map.Entry<Ticket, Integer> ticketEntry : ticketStorage.getTickets().entrySet()) {
             int numberOfTicketsToPrint = ticketEntry.getValue();
             while (numberOfTicketsToPrint>0) {
                 messagePrinter.printMessage(ticketEntry.getKey().toString());
                 paperContainerState -= 1;
+                numberOfTicketsToPrint -= 1;
             }
         }
     }
