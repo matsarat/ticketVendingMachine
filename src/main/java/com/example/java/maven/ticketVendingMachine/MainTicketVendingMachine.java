47c9aef039d27b782dd96e89a4f40f143c181db2
matsarat <mat.saratowicz@gmail.com>
2021-02-09 17:27:37 +0100

    Apply basic version of ticket vending machine

--- src/main/java/com/example/java/maven/ticketVendingMachine/MainTicketVendingMachine.java
+++ src/main/java/com/example/java/maven/ticketVendingMachine/MainTicketVendingMachine.java
@@ -1,4 +1,42 @@
 package com.example.java.maven.ticketVendingMachine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainTicketVendingMachine {
+
+    public static void main(String[] args) {
+        MessagePrinter messagePrinter = new MessagePrinter();
+        UserInputProvider userInputProvider = new UserInputProvider();
+        TicketPrinter ticketPrinter = new TicketPrinter(100, messagePrinter);
+        TicketStorage ticketStorage = new TicketStorage();
+
+        Map<Coin, Integer> mainCoins = new HashMap<>();
+        Map<Coin, Integer> tempCoins = new HashMap<>();
+        Map<Coin, Integer> oddCoins = new HashMap<>();
+
+
+        CoinStorage tempCoinStorage = new CoinStorage(tempCoins, messagePrinter);
+        CoinStorage mainCoinStorage = new CoinStorage(mainCoins, messagePrinter);
+        CoinStorage oddMoneyStorage = new CoinStorage(oddCoins, messagePrinter);
+
+
+        TicketVendingMachine ticketVendingMachine = new TicketVendingMachine(
+                ticketPrinter,
+                ticketStorage,
+                mainCoinStorage,
+                tempCoinStorage,
+                oddMoneyStorage,
+                messagePrinter,
+                userInputProvider);
+
+        ticketVendingMachine.fillMainCoinStorage();
+        ticketVendingMachine.getTicketsFromUser();
+        ticketStorage.getValueOfTicketsInStorage();
+        ticketVendingMachine.getPaymentFromUser();
+        ticketVendingMachine.giveBackOddMoney();
+        CoinStorage.moveCoinsFromTempToMainCoinStorage(mainCoinStorage, tempCoinStorage);
+        ticketPrinter.printTicketsFromTicketStorage(ticketStorage);
+
+    }
 }
