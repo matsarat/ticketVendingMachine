c6bd5b2a43d7e9bc9f03c602b47be2cd79f6ad3d
matsarat <mat.saratowicz@gmail.com>
2021-02-13 16:52:41 +0100

    Move `getCoin` and `getTicket` methods from TicketVendingMachine class

--- src/main/java/com/example/java/maven/ticketVendingMachine/UserInputProvider.java
+++ src/main/java/com/example/java/maven/ticketVendingMachine/UserInputProvider.java
@@ -3,9 +3,30 @@ package com.example.java.maven.ticketVendingMachine;
 import java.util.Scanner;
 
 public class UserInputProvider {
+    MessagePrinter messagePrinter = new MessagePrinter();
 
     public String getString() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
+    }
+
+    public Coin getCoin() {
+        String userInput = getString();
+        try {
+            return Coin.matchUserInput(userInput.toUpperCase());
+        } catch (IllegalArgumentException exception) {
+            messagePrinter.printError(exception.getMessage());
+            return getCoin();
+        }
+    }
+
+    public Ticket getTicket() {
+        String chosenTicket = getString();
+        try {
+            return Ticket.matchUserInput(chosenTicket.toUpperCase());
+        } catch (IllegalArgumentException exception) {
+            messagePrinter.printError(exception.getMessage());
+            return getTicket();
+        }
     }
 }
