3f3fce612b3959c4ed71b1ec7c1b90e95af71a34
matsarat <mat.saratowicz@gmail.com>
2021-02-18 21:07:48 +0100

    Add tests for giveCoinsBack method

--- src/test/java/com/example/java/maven/ticketVendingMachine/CoinStorageTest.java
+++ src/test/java/com/example/java/maven/ticketVendingMachine/CoinStorageTest.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import static org.mockito.BDDMockito.then;
+
 import java.util.HashMap;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
 
 
 public class CoinStorageTest {
@@ -61,5 +64,25 @@ public class CoinStorageTest {
 
         assertThat(destinationCoinStorage.getCoins().size()).isEqualTo(6);
         assertThat(destinationCoinStorage.getValueOfCoinsInStorage()).isEqualTo(880);
+    }
+
+    @Test
+    void shouldGiveCoinsBackToUser() {
+
+//        given
+        primaryCoinStorage.fillCoinStorageWithGivenNumberOfCoinsForDenomination(1);
+
+//        when
+        primaryCoinStorage.giveCoinsBack();
+
+//        then
+        assertThat(primaryCoinStorage.getCoins().size()).isEqualTo(0);
+        then(messagePrinter).should(times(1)).printMessage("Coin 5 PLN");
+        then(messagePrinter).should(times(1)).printMessage("Coin 2 PLN");
+        then(messagePrinter).should(times(1)).printMessage("Coin 1 PLN");
+        then(messagePrinter).should(times(1)).printMessage("Coin 0,50 PLN");
+        then(messagePrinter).should(times(1)).printMessage("Coin 0,20 PLN");
+        then(messagePrinter).should(times(1)).printMessage("Coin 0,10 PLN");
+
     }
 }
